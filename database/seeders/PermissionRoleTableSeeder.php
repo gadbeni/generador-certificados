2d9e66529bd7c46cf6dc3d50e24fce5b3c8aea60
Andoni1Barba <Andoni1barba@gmail.com>
2023-10-26 16:24:42 -0400

    roles actualizados version 1.0.1

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -22,5 +22,25 @@ class PermissionRoleTableSeeder extends Seeder
         $role->permissions()->sync(
             $permissions->pluck('id')->all()
         );
+
+        $role = Role::where('name', 'courses_admin')->firstOrFail();
+
+        $permissions = Permission::whereRaw('table_name = "admin" or
+                                            table_name = "people" or
+
+                                            `key` = "browse_admin" or
+                                            `key` = "browse_courses" or
+                                            `key` = "read_courses" or
+                                            `key` = "edit_courses" or
+                                            `key` = "add_courses" or
+
+                                            `key` = "browse_course_person" or
+                                            `key` = "read_course_person" or
+                                            `key` = "edit_course_person" or
+                                            `key` = "add_course_person"')->get();
+
+        $role->permissions()->sync(
+            $permissions->pluck('id')->all()
+        );
     }
 }
--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -25,5 +25,11 @@ class RolesTableSeeder extends Seeder
                 'display_name' => __('voyager::seeders.roles.user'),
             ])->save();
         }
+        $role = Role::firstOrNew(['name' => 'courses_admin']);
+        if (!$role->exists) {
+            $role->fill([
+                'display_name' => __('administrador De Cursos'),
+            ])->save();
+        }
     }
 }
